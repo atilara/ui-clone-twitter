4d97f255494947727c9c5939ab916567e66f389e
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-07-08 18:40:03 -0300

    Created the global stylization and the layout that will wrap the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import Layout from './components/Layout';
+import GlobalStyles from './styles/GlobalStyles';
+
 function App() {
 	return (
-		<h1>Hello world</h1>
+		<>
+			<Layout />
+			<GlobalStyles />
+		</>
 	);
 }
 
